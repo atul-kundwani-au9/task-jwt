6ca3323d6ac6cf73880e64eac2aa1722b8b643bb
atul kundwani <atul_0012@yahoo.co.in>
2023-08-28 23:41:50 +0530

    task

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
+
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -9,9 +12,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  
 });
+
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
